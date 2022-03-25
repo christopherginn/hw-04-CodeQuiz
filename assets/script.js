28e7d5bc2bcd4ff342823a2ac038b1965896d5c7
christopherginn <hey.im.chris@live.com>
2022-03-25 18:42:42 -0400

    index.html has been populated with elements. script.js has been populated with variables

---
+++ assets/script.js
@@ -0,0 +1,20 @@
+var highscoresEl = document.querySelector("#highscores");
+var timeLeftEl = document.querySelector("#timeleft");
+var welcomeScreenEl = document.querySelector("#welcomeScreen");
+var questionScreenEl = document.querySelector("#questionScreen");
+var endScreenEl = document.querySelector("#endScreen");
+var highscoresScreenEl = document.querySelector("#highscoresScreen");
+var startBtnEl = document.querySelector("#start");
+var nextBtnEl = document.querySelector("#next");
+var submitAnswersBtnEl = document.querySelector("#submitAnswers");
+var backWelcomeBtnEl = document.querySelector("#backtowelcome");
+
+function init() {
+    setEventListeners();
+}
+
+function setEventListeners() {
+    
+}
+
+init();
